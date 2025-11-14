06a91bed0f6fb81dc81eb4259e91294d470350c3
deebvoiceboy34 <lucaspaleacu@yahoo.com>
2025-11-14 14:07:34 -0500

    added classes

---
+++ core/src/main/java/it/thesquad/foodtruck/Order.java
@@ -0,0 +1,23 @@
+package it.thesquad.foodtruck;
+
+public class Order {
+    private Food MainEntree;
+    private Food SideDish;
+    private Food DessertsDish;
+    private Food Drink;
+
+    public Food getMainEntree() {
+        return MainEntree;
+
+    }
+    public Food getSideDish() {
+        return SideDish;
+    }
+    public Food getDessertsDish() {
+        return DessertsDish;
+    }
+    public Food getDrink() {
+        return Drink;
+    }
+
+}
