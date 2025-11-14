1d18f4a44abc2d3c9c189356449b24a9d07d778c
MEME-KING16 <seb.filip.ca@gmail.com>
2025-11-14 14:32:42 -0500

    Added Foods enum

---
+++ core/src/main/java/it/thesquad/foodtruck/Drinks.java
@@ -0,0 +1,12 @@
+package it.thesquad.foodtruck;
+
+public enum Drinks {
+    WATER,
+    SWEET_TEA,
+    JUICE,
+    SMOOTHIE,
+    WINE,
+    BEER,
+    COCKTAIL,
+    LEMONADE
+}
---
+++ core/src/main/java/it/thesquad/foodtruck/Foods.java
@@ -0,0 +1,34 @@
+package it.thesquad.foodtruck;
+
+public enum Foods {
+    // Mains
+    BURGER("Burger"),
+    RIBS("Ribs"),
+    BRISKET("Brisket"),
+    SAUSAGES("Sausages"),
+    POUTINE("Poutine"),
+    Korean_Fried_Chicken("Korean Fried Chicken"),
+
+
+    // Sides
+    FRIES("Fries"),
+    SOUP("Soup"),
+    ONION_RINGS("Onion Rings"),
+    COLESLAW("Coleslaw"),
+    MAC_N_CHEESE("Mac n Cheese"),
+    SALAD("Salad"),
+
+    // Desserts
+    PEACH_COBBLER("Peach Cobbler"),
+    PIE("Pie"),
+    COOKIES("Cookies"),
+    ICE_CREAM("Ice Cream"),
+    CARROT_CAKE("Carrot Cake");
+
+    String name;
+
+    public Food(String someShit) {
+        this.name = someShit;
+    }
+
+}
