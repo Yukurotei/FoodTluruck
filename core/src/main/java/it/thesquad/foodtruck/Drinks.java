35ce6f02a30851e2d58d5ba7e758e1f76bb21a6a
MEME-KING16 <seb.filip.ca@gmail.com>
2025-11-14 14:35:02 -0500

    added drinks enum

--- core/src/main/java/it/thesquad/foodtruck/Drinks.java
+++ core/src/main/java/it/thesquad/foodtruck/Drinks.java
@@ -1,12 +1,18 @@
 package it.thesquad.foodtruck;
 
 public enum Drinks {
-    WATER,
-    SWEET_TEA,
-    JUICE,
-    SMOOTHIE,
-    WINE,
-    BEER,
-    COCKTAIL,
-    LEMONADE
+    WATER("Water"),
+    SWEET_TEA("Sweet Tea"),
+    JUICE("Juice"),
+    SMOOTHIE("Smoothie"),
+    WINE("Wine"),
+    BEER("Beer"),
+    COCKTAIL("Cocktail"),
+    LEMONADE("Lemonade");
+
+    String name;
+
+    private Drinks(String someShit) {
+        this.name = someShit;
+    }
 }
