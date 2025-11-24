5a6af84026110fddb1d28351aaa925bd7579ddf5
MEME-KING16 <seb.filip.ca@gmail.com>
2025-11-24 12:41:22 -0500

    added customer class

--- core/src/main/java/it/thesquad/foodtruck/customers/Customer.java
+++ core/src/main/java/it/thesquad/foodtruck/customers/Customer.java
@@ -1,7 +1,26 @@
 package it.thesquad.foodtruck.customers;
 
+import it.thesquad.foodtruck.Order;
+import it.thesquad.foodtruck.reputation.Reputation;
+import it.thesquad.foodtruck.reputation.Review;
+
 public class Customer {
-    Customer() {
-        
+    Order order;
+    Review review;
+    Customer(Order order) {
+        this.order = order;   
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setReview(Review review) {
+        this.review = review;
+        Reputation.addReview(review);
+    }
+
+    public Review getReview() {
+        return review;
     }
 }
