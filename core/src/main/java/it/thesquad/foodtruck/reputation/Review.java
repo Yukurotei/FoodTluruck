31e86a688c5fd52234be68047504761ad947f65b
MEME-KING16 <seb.filip.ca@gmail.com>
2025-11-24 11:34:42 -0500

    stuff2.0

--- core/src/main/java/it/thesquad/foodtruck/reputation/Review.java
+++ core/src/main/java/it/thesquad/foodtruck/reputation/Review.java
@@ -2,11 +2,18 @@ package it.thesquad.foodtruck.reputation;
 
 public class Review {
     int rating;
-    Review() {
+    String reviewText;
+    Review(int rating, String reviewText) {
+        this.rating = rating;
+        this.reviewText = reviewText;
 
     }
 
     public int getRating() {
         return rating;
+    }
+
+    public String getReviewText() {
+        return reviewText;
     }
 }
