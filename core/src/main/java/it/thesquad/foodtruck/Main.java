c1479724dd06549d559dd072e29f10b563b1fa24
MEME-KING16 <115302822+MEME-KING16@users.noreply.github.com>
2025-11-27 16:32:52 -0500

    JEFF WHAT THE GOON

--- core/src/main/java/it/thesquad/foodtruck/Main.java
+++ core/src/main/java/it/thesquad/foodtruck/Main.java
@@ -44,7 +44,7 @@ public class Main extends ApplicationAdapter {
         Grill grill = new Grill(Utils.resizeTo(new Texture("grill.png"), 50), 40, 40, 20, 20);
         Cutter cuttingBoard = new Cutter(Utils.resizeTo(new Texture("cuttingBoardSimple.png"), 50), 100, 100, 20, 20);
         Fridge fridge = new Fridge(Utils.resizeTo(new Texture("fridgeTopDown.png"), 50), 360, 360, 20, 20);
-        DeepFryer fryer = new DeepFryer(Utils.resizeTo(new Texture("fryerUI.png"), 50), 200, 200, 20, 20);
+        DeepFryer fryer = new DeepFryer(Utils.resizeTo(new Texture("fryerUI.png"), 20), 200, 200, 20, 20);
         Sprite interactionSprite = new Sprite(Utils.resizeTo(new Texture("interact.png"), 50), 0, 0, true);
         interactionSprite.setVisible(false);
         new Player(new Texture("player.png"), interactionSprite);
